fc44749ed915b235e383265d7d7d381cd0393353
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-04-08 11:55:40 -0300

    Agregué las acciones del reducer y cambié algunos nombres de las actionTypes

--- PI-Videogames-main/client/src/redux/reducer.js
+++ PI-Videogames-main/client/src/redux/reducer.js
@@ -1,19 +1,22 @@
 import { GET_ALL_VIDEOGAMES,
     GET_DETAIL_VIDEOGAME,
-    CLEAN_DETAIL,
     GET_ALL_GENRES,
-    GET_BY_NAME,
+    CLEAN_DETAIL,
     FILTER_BY_GENRE,
-    FILTER_BY_ALPHA,
-    FILTER_ALPHA_REV,
-    FILTER_BY_PLATFORMS,
-    FILTER_BY_RATING,
+    FILTER_BY_SOURCE,
+    SEARCH_VIDEOGAMES,
+    ORDER_VIDEOGAMES,
     POST_VIDEOGAMES } from "./actionTypes";
+
 
 const initialState = {
     allVideogames: [],
+    detailVideogame : [],
     allGenres:[],
     copyAllVideogames: [],
+    filterGenre: "defect",
+    filterSource: "defect" ,
+    videogameCrate: {}
     
 }
 
@@ -24,56 +27,133 @@ function rootReducer (state = initialState, action){
                 ...state,
                 allVideogames: action.payload,
                 copyAllVideogames: action.payload
-        }
+            }
         case GET_DETAIL_VIDEOGAME:
             return {
                 ...state,
+                detailVideogame: action.payload
+            }
+        case CLEAN_DETAIL:
+            return {
+                ...state,
+                detailVideogame: {}
+            }
+        case SEARCH_VIDEOGAMES:
+            return {
+                ...state,
                 allVideogames: action.payload
-        }
+            }
         case GET_ALL_GENRES:
-        return {
-            ...state,
-            allVideogames: action.payload
-        }
-        case GET_BY_NAME:
-        return {
-            ...state,
-            allVideogames: action.payload
-        }
+            return {
+                ...state,
+                allGenres: action.payload
+            }
         case FILTER_BY_GENRE:
-        return {
-            ...state,
-            allVideogames: action.payload
+            if (filterSource !== 'defect'){
+                const filterBySource = 
+                    state.filterSource === 'api'? 
+                    [ ...state.copyAllVideogames].filter((videogame) => !isNaN(videogame.id)):
+                    [...state.copyAllVideogames].filter((videogame) => isNaN(videogame.id));
+                const filterByGenre = 
+                    action.payload === 'defect'?
+                    filterBySource :
+                    filterBySource.filter((videogame) => videogame.genres.some(genre => genre.name === action.payload ));
+                    return {
+                        ...state,
+                        allVideogames: filterByGenre,
+                        filterGenre: action.payload
+                    }
+            }else {
+                const filterByGenre = action.payload === 'defect' ?
+                [...state.copyAllVideogames]:
+                [...state.copyAllVideogames.filter((videogame) => videogame.genre.some(genre => genre.name === action.payload))]
+                return {
+                    ...state,
+                    allVideogames: filterByGenre,
+                    filter: action.payload
+                }
+            }
+        case FILTER_BY_SOURCE :
+            if(state.filterGenre !== 'defect'){
+                const filterByGenre = 
+                    [...state.copyAllVideogames].filter((videogame)=>videogame.genres.some((genres) => genres.name === state.filterGenre))
+                if(action.payload === 'api'){
+                    const filterBySource = filterByGenre.filter((videogame) => !isNaN(videogame.id))
+                    return {
+                        ...state,
+                        allVideogames: filterBySource,
+                        filterBySource: action.payload
+                    }
+                }else {
+                    const filterBySource = 
+                        action.payload === 'defect'?
+                        filterByGenre :
+                        filterByGenre.filter((videogame) => isNaN(videogame.id))
+                        return {
+                            ...state,
+                            allVideogames: filterBySource,
+                            filterBySource: action.payload
+                        }
+                }
+            }else {
+                if(action.payload === 'api'){
+                const filterBySource = 
+                    action.payload === 'defect'? 
+                    [...state.copyAllVideogames]:
+                    [...state.copyAllVideogames].filter((videogame)=> !isNaN(videogame.id)) :
+                    return {
+                        ...state,
+                        allVideogames: filterBySource,
+                        filterSource: action.payload
+                    }
+                }else {
+                    const filterBySource = 
+                        action.payload === 'defect' ?
+                        [...state.copyAllVideogames] :
+                        [...state.copyAllVideogames].filter((videogame) => isNaN(videogame.id))
+                    return {
+                        ...state,
+                        allVideogames: filterBySource,
+                        filterBySource: action.payload
+                    }    
+                }
+            }        
+        case ORDER_VIDEOGAMES:
+            switch(action.payload){
+                case "asc":
+                    const videoAsc = [...state.allVideogames].sort((a,b) => a.name.localeCompare(b.name))
+                    return {
+                        ...state,
+                        allVideogames : videoAsc
+                    }
+                case "desc":
+                    const videoDes = [...state.allVideogames].sort((a,b) => b.name.localeCompare(a.name))
+                    return {
+                        ...state,
+                        allVideogames : videoDes
+                    }
+                case "higherRating":
+                    const higherRating = [...state.allVideogames].sort((a,b) => b.rating - a.rating)
+                    return {
+                        ...state,
+                        allVideogames : higherRating
+                    }
+                case "lowerRating":
+                    const lowerRating = [...state.allVideogames].sort((a,b) => a.rating - b.rating)
+                    return {
+                        ...state,
+                        allVideogames : lowerRating
+                    }
+                default:
+                    return {
+                        ...state
+                    }
         }
-        case FILTER_BY_ALPHA :
-        return {
-            ...state,
-            allVideogames: action.payload
-        }        
-        case FILTER_ALPHA_REV:
-        return {
-            ...state,
-            allVideogames: action.payload
-        }
-        case FILTER_BY_PLATFORMS:
-            return {
-                ...state,
-                allVideogames: action.payload
-        }
-        case FILTER_BY_RATING:
-            return {
-                ...state,
-                allVideogames: action.payload
-        }
-        case  CLEAN_DETAIL:
-            return {
-                ...state,
-                allVideogames: action.payload
-        }
+
         case POST_VIDEOGAMES:
             return {
                 ...state,
-                allVideogames: action.payload
+                videogameCrate: action.payload
         }        
         default:
             return state
