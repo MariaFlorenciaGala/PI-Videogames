066332cd0eb51a5ca15882740a0958302b3e5207
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-04-10 18:21:49 -0300

    Cree los filtros

--- PI-Videogames-main/client/src/components/filters/filters.jsx
+++ PI-Videogames-main/client/src/components/filters/filters.jsx
@@ -1,9 +1,60 @@
-
 //import "./filters.css";
+//ACTIONS
+import {getAllGenres, filterByGenre, filterBySource, orderVideogameS } from "../../redux/actions";
+
+//REACT REDUX
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Filters = () => {
+  const dispatch = useDispatch();
+  const allGenres = useSelector((state) => state.allGenres)
+
+  const handleChangeGenre = (e) => {
+    dispatch(filterByGenre(e.target.value))
+  }
+
+  const handleChangeSource = (e) => {
+    dispatch(filterBySource(e.target.value))
+  }
+  const handleChangeRating = (e) => {
+    dispatch(orderVideogameS(e.target.value))
+  }
+
+  const handleChangeAlpha = (e) => {
+    dispatch(orderVideogameS(e.target.value))
+  }
+
+  useEffect(() => {
+    dispatch(getAllGenres())
+  },[dispatch])
+
     return (
-      <div>Filters</div>
+      <div>
+        <label>Géneros</label>
+        <select name='filterGenre' defaultValue='defect' id='filterGenre' onChange={handleChangeGenre}>
+          <option value='defect'>Todos</option>
+          {allGenres.map((genre) => {
+            return <option key={genre.id} value={genre.name}>{genre.name}</option>
+          })}
+        </select>        
+        <label>Fuente</label>
+        <select name='filterSource' defaultValue='defect' id='filterSource' onChange={handleChangeSource}>
+          <option value='defect'>Todos</option>
+          <option value='bdd'>BDD</option>
+          <option value='api'>API</option>
+        </select>
+        <label>Rating</label>
+        <select name='filterRating' defaultValue='defect' id='filterRating' onChange={handleChangeRating}>
+          <option value='higherRat'>higherRat</option>
+          <option value='lowerRat'>lowerRat</option>
+        </select>
+        <label>Orden alfabético</label>
+        <select name='filterAlpha' defaultValue='defect' id='filterAlpha' onChange={handleChangeAlpha}>
+          <option value='asc'>A-Z</option>
+          <option value='desc'>Z-A</option>
+        </select>
+      </div>
     )
     };
   
