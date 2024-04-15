d455261677676e1d84b09877c90d8b3265157989
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-04-15 11:46:47 -0300

    Agregué el codigo para crear un nuevo videojuego

--- PI-Videogames-main/client/src/views/newVideogame/newVideogame.jsx
+++ PI-Videogames-main/client/src/views/newVideogame/newVideogame.jsx
@@ -1,26 +1,287 @@
+import "./newVideoGame.styles.css";
+//REDUX-REACT
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-//import "./newVideoGame.css";
+//ACTIONS
+import {
+  getAllGenres,
+  postVideogame,
+} from "../../redux/actions";
+
+//VALIDACIONES
+import validation from "../newVideogame/validation";
 
 const NewVideogame = () => {
-    return (
-      <div>
-        <div class="form-container">
-              <form class="form">
-                <div class="form-group">
-                  <label for="email">Company Email</label>
-                  <input required="" name="email" id="email" type="text" />
-                </div>
-                <div class="form-group">
-                  <label for="textarea">How Can We Help You?</label>
-                  <textarea required="" cols="50" rows="10" id="textarea" name="textarea">          </textarea>
-                </div>
-                <button type="submit" class="form-submit-btn">Submit</button>
-              </form>
-        </div>        
-      </div>
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const genresApi = useSelector((state) => state.allGenres);
 
-    )
-    };
-  
-  export default NewVideogame;
-  
+  const platformsAPI = [
+    "PC",
+    "PlayStation 5",
+    "PlayStation 4",
+    "PlayStation 3",
+    "PlayStation 2",
+    "PlayStation",
+    "Wii U",
+    "Xbox One",
+    "Nintendo Switch",
+    "iOS",
+    "Android",
+    "macOS",
+    "Linux",
+    "Xbox 360",
+    "SEGA Master System",
+  ];
+
+  useEffect(() => {
+    dispatch(getAllGenres());
+  }, [dispatch]);
+
+  const [valor, setValor] = useState({
+    name: "",
+    image: "",
+    released: "",
+    description: "",
+    rating: "5",
+    genresName: [],
+    genres: [],
+    platforms: [],
+  });
+
+  const [error, setError] = useState({
+    name: "",
+    image: "",
+    released: "",
+    description: "",
+    rating: "5",
+    genresName: "",
+    genres: "",
+    platforms: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValor({ ...valor, [name]: value });
+    setError(
+      validation({
+        ...valor,
+        [name]: value,
+      })
+    );
+  };
+  const handleChangePlatforms = (event) => {
+    const { value } = event.target;
+    if (valor.platforms.length < 6) {
+      if (valor.platforms.length > 0) {
+        const repetido = valor.platforms.every(
+          (platform) => platform !== value
+        );
+        if (repetido) {
+          setValor({
+            ...valor,
+            platforms: [...valor.platforms, value],
+          });
+        }
+      } else {
+        setValor({
+          ...valor,
+          platforms: [...valor.platforms, value],
+        });
+      }
+    }
+  };
+
+  const handleChangeGenres = (event) => {
+    const { value } = event.target;
+    if (valor.genresName.length < 6) {
+      if (valor.genresName.length > 0) {
+        const genresAPICoinciden = genresApi.filter(
+          (genre) => value === genre.name
+        );
+        const idGenres = genresAPICoinciden.map((genre) => genre.id);
+        const repetido = valor.genresName.every((genre) => genre !== value);
+        if (repetido) {
+          setValor({
+            ...valor,
+            genresName: [...valor.genresName, value],
+            genres: [...valor.genres, idGenres],
+          });
+        }
+      } else {
+        const genresAPICoinciden = genresApi.filter(
+          (genre) => value === genre.name
+        );
+        const idGenres = genresAPICoinciden.map((genre) => genre.id);
+        setValor({
+          ...valor,
+          genresName: [...valor.genresName, value],
+          genres: [...valor.genres, idGenres],
+        });
+      }
+    }
+  };
+
+  const handleClosePlatform = (name) => {
+    const platformFilter = valor.platforms.filter((platform) => {
+      return platform !== name;
+    });
+    setValor({
+      ...valor,
+      platforms: [...platformFilter],
+    });
+  };
+
+  const handleCloseGenre = (name) => {
+    const genresFilter = valor.genresName.filter((genre) => {
+      return genre !== name;
+    });
+    const genresAPICoinciden = genresApi.filter((genre) =>
+      genresFilter.includes(genre.name)
+    );
+    const idGenres = genresAPICoinciden.map((genre) => genre.id);
+    setValor({
+      ...valor,
+      genresName: [...genresFilter],
+      genres: [...idGenres],
+    });
+  };
+
+  //Función onSubmit
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dispatch(postVideogame(valor));
+    navigate("/home");
+  };
+
+  return (
+    <form className="container-form" onSubmit={onSubmit}>
+      <div className="container-form-part line">
+        <u>
+          <h3>Completa los datos del nuevo videojuego:</h3>
+        </u>
+        <div className="container-form-inputs">
+          <div className="form-input">
+            <label htmlFor="name">Nombre:</label>
+            <input
+              type="text"
+              name="name"
+              value={valor.name}
+              onChange={handleChange}
+            />
+          </div>
+          <p className="error-menssage">{error.name}</p>
+          <div className="form-input">
+            <label htmlFor="image">Image(url):</label>
+            <input
+              type="text"
+              name="image"
+              value={valor.image}
+              onChange={handleChange}
+            />
+          </div>
+          <p className="error-menssage">{error.image}</p>
+          <div className="form-input">
+            <label htmlFor="released">Fecha de lanzamiento:</label>
+            <input
+              type="date"
+              name="released"
+              value={valor.released}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input-textarea">
+            <label htmlFor="description">Descripción:</label>
+            <textarea
+              name="description"
+              cols="45"
+              rows="5"
+              value={valor.description}
+              onChange={handleChange}
+            ></textarea>
+            <p className="error-menssage">{error.description}</p>
+          </div>
+        </div>
+      </div>
+      <div className="container-form-part ">
+        <div className="form-select">
+          <div>
+            <label htmlFor="platforms">Platforms:</label>
+            <select
+              name="platforms"
+              id="platforms"
+              defaultValue=""
+              onChange={handleChangePlatforms}
+            >
+              <option value=""></option>
+              {platformsAPI.map((platform) => {
+                return <option value={platform}>{platform}</option>;
+              })}
+            </select>
+          </div>
+          <div className="wrap">
+          <option value=""></option>
+            {valor.platforms.map((platform, index) => (
+              <p key={index} onClick={() => handleClosePlatform(platform)}>{platform}</p>
+            ))}
+          </div>
+          <p className="error-menssage">{error.platforms}</p>
+        </div>
+        <div className="form-select">
+          <div>
+            <label htmlFor="genres">Genres:</label>
+            <select
+              name="genres"
+              id="genres"
+              defaultValue=""
+              onChange={handleChangeGenres}
+            >
+              {genresApi.map((genre,index) => {
+                return (
+                  <option key={index} value={genre.name}>
+                    {genre.name}
+                  </option>
+                );
+              })}
+            </select>
+            <br />
+          </div>
+          <div className="wrap">
+          <option value=""></option>
+            {valor.genresName.map((genre, index) => (
+              <p key={index} onClick={() => handleCloseGenre(genre)}>{genre}</p>
+            ))}
+          </div>
+          <p className="error-menssage">{error.genres}</p>
+        </div>
+        <div className="form-range">
+          <label htmlFor="rating">Rating:</label>
+          <input
+            type="range"
+            name="rating"
+            min="1"
+            max="10"
+            step="0.01"
+            value={valor.rating}
+            onChange={handleChange}
+          />
+          <span>{valor.rating}</span>
+          <p className="error-menssage">{error.rating}</p>
+        </div>
+        <button
+          type="submit"
+          className="button-form"
+          disabled={Object.values(error).some(
+            (errorMessage) => errorMessage !== ""
+          )}
+        >
+          Create
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default NewVideogame;
