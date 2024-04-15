dd521fdf63e2e0500beb9c236b8d340f992ff99e
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-04-15 16:58:18 -0300

    agregué estilos a la landing

--- PI-Videogames-main/client/src/views/landing/landing.jsx
+++ PI-Videogames-main/client/src/views/landing/landing.jsx
@@ -1,15 +1,17 @@
-//import "./landing.css";
+import "./landing.styles.css";
+import videoBG from '../../assets/videoBG.mp4'
 import { Link } from 'react-router-dom'
 
-//COMPONENTES
-import Home from "../home/home";
 
 const Landing = () => {
     return (
-      <div>
-        <div>Landing</div>
-        <Link to='/home'><button>Home</button>
-</Link>
+      <div className="landing">
+        <div  className="landingTitle">
+          <h1>PI VIDEOGAMES</h1><hr />
+          <p>By Maria Florencia Batalla Córdoba</p>
+          <Link to='/home'><button className="landingButton">Home</button></Link>
+        </div>
+        <video src={videoBG} autoPlay loop muted ></video>
       </div>
     )
     };
