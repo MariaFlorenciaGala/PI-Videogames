de5018632a132d8d2a4f3254c24d6a1991fdf3e5
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-04-17 17:05:04 -0300

    Agregué los iconos a la nav

--- PI-Videogames-main/client/src/components/nav/nav.jsx
+++ PI-Videogames-main/client/src/components/nav/nav.jsx
@@ -1,21 +1,43 @@
+import './nav.styles.css';
 
-import "./nav.styles.css";
-
-//REDUX - REACT
+//REACT
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
+import NewVideogame from '../../views/newVideogame/newVideogame';
+//iconos
+import { FaHome } from "react-icons/fa";
+import { IoPersonCircleSharp } from "react-icons/io5";
+import { MdOutlineVideogameAsset } from "react-icons/md";
+
+
 
 
 const Nav = () => {
-    return (
-      <div>
-        <nav className="allNav">
-            <Link to={'/home'}>Home</Link>
-            <Link to={'/aboutMe'}>Sobre mi</Link>
-            <Link to={'/create'}>Nuevo videojuego</Link>
-        </nav>
-      </div>
-    )
-    };
-  
-  export default Nav;
-  
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  return (
+    <div>
+      <nav className="allNav">
+        <Link to={"/"}><FaHome className='icono' /> Home</Link>
+        <Link to={"/aboutMe"}><IoPersonCircleSharp className='icono'/>Sobre mi</Link>
+        <Link onClick={toggleModal}><MdOutlineVideogameAsset className='icono'/>Crear videogame</Link>
+      </nav>
+        {
+          showModal && (
+            <div className="modalmask">
+              <div className="modalbox">
+                <button className="close" onClick={toggleModal}>X</button>
+                <NewVideogame />
+              </div>
+            </div>
+          )
+        }
+    </div>
+  );
+};
+
+export default Nav;
