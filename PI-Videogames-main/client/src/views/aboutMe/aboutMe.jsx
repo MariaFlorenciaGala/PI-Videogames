ed890f72baa9d20d345bf58e524fa6a73a5855dd
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-04-17 17:05:54 -0300

    Hice la pagina about me y le agregué estilos

--- PI-Videogames-main/client/src/views/aboutMe/aboutMe.jsx
+++ PI-Videogames-main/client/src/views/aboutMe/aboutMe.jsx
@@ -1,9 +1,19 @@
-
-//import "./aboutMe.css";
+import "./aboutMe.styles.css";
 
 const AboutMe = () => {
     return (
-      <div>AboutMe</div>
+      <div className="container">
+        <h2>María Florencia Batalla Córdoba</h2>        
+        <h3>Estudiante de FullStack development</h3>
+        <div className="containerMe">
+          <div>
+          <p>Como estudiante en Henry, mi objetivo es continuar aprendiendo y creciendo como desarrollador de software. Me apasiona la idea de contribuir a proyectos significativos y ser parte de una comunidad vibrante de profesionales de la tecnología</p>
+          <p>Lo que más me motiva es la oportunidad de enfrentar desafíos técnicos, aprender algo nuevo cada día y compartir conocimientos con otros entusiastas de la programación. Estoy emocionado por lo que depara el futuro y estoy comprometido a seguir creciendo en este apasionante campo.</p>
+          </div>
+          <img src="https://ca.slack-edge.com/TPRS7H4PN-U063PHP9J81-2a8df898104c-512"></img>
+          </div>
+        
+      </div>
     )
     };
   
